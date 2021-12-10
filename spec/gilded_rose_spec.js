cc63102c1c64123813044696693631bba5f02d19
Ashraf <ashraf@Ashrafs-Air.home>
2021-12-10 16:22:16 -0500

    Checking Sulfuras, Hand of Ragnaros quality - test fail

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -123,5 +123,12 @@ describe("Gilded Rose,", function() {
         }
       }
     });
+    it("for Sulfuras, Hand of Ragnaros nothing changes", function() {
+      for(let i=0;i<gilded_rose.items.length;i++) {
+        if (gilded_rose.items[i].name == "Sulfuras, Hand of Ragnaros") {
+          assert(gilded_rose.items[i].quality < 0 && gilded_rose.items[i].quality < 50);
+        }
+      }
+    });
   });
 });
