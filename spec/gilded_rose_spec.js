f9d898f00ad74f73685b9739beff2cea3408569a
Ashraf <ashraf@Ashrafs-Air.home>
2021-12-10 15:53:35 -0500

    Checking Aged Brie sell_in vs quality - test pass

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -63,8 +63,8 @@ describe("Gilded Rose,", function() {
     it("for Aged Brie only sell_in lowers quality increase", function() {
       for(let i=0;i<gilded_rose.items.length;i++) {
         if (gilded_rose.items[i].name == "Aged Brie") {
-          assert(gilded_rose.items[i].sell_in > 2);
-          assert(gilded_rose.items[i].quality < 0);
+          assert(gilded_rose.items[i].sell_in < 2);
+          assert(gilded_rose.items[i].quality > 0);
         }
       }
     });
