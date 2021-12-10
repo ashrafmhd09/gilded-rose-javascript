cb9159f41ec7210a035de474142d197cbabb0398
Ashraf <ashraf@Ashrafs-Air.home>
2021-12-10 15:16:09 -0500

    checking Dexterity Vest in the inventory - test pass

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -11,7 +11,7 @@ describe("Gilded Rose,", function() {
     it("+5 Dexterity Vest", function() {
       for(let i=0;i<gilded_rose.items.length;i++) {
         if (gilded_rose.items[i].name == "+5 Dexterity Vest") {
-          assert(gilded_rose.items[i].name != "+5 Dexterity Vest")
+          assert(gilded_rose.items[i].name == "+5 Dexterity Vest")
         }
       }
     });
