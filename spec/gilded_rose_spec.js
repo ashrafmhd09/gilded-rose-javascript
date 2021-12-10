f99a58db0f3a89a7044c78c653a7cdc74d1f77de
Ashraf <ashraf@Ashrafs-Air.home>
2021-12-10 15:15:18 -0500

    Checking Dexterity Vest is in the inventory - test fail

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -8,5 +8,12 @@ describe("Gilded Rose,", function() {
     it("items in the inventory", function() {
       assert(gilded_rose.items.length > 0);
     });
+    it("+5 Dexterity Vest", function() {
+      for(let i=0;i<gilded_rose.items.length;i++) {
+        if (gilded_rose.items[i].name == "+5 Dexterity Vest") {
+          assert(gilded_rose.items[i].name != "+5 Dexterity Vest")
+        }
+      }
+    });
   });
 });
