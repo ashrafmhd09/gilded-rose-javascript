01046c6d2754fe490f6f593cd467e87332227537
Ashraf <ashraf@Ashrafs-Air.home>
2021-12-10 15:43:57 -0500

    Checking Conjured Mana Cake in the inventory - test pass

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -46,7 +46,7 @@ describe("Gilded Rose,", function() {
     it("Conjured Mana Cake", function() {
       for(let i=0;i<gilded_rose.items.length;i++) {
         if (gilded_rose.items[i].name == "Conjured Mana Cake") {
-          assert(gilded_rose.items[i].name != "Conjured Mana Cake");
+          assert(gilded_rose.items[i].name == "Conjured Mana Cake");
         }
       }
     });
