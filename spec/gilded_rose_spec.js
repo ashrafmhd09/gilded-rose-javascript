a9d731b4f482a9a2fa6b0b59ca491f76b1c0105f
Ashraf <ashraf@Ashrafs-Air.home>
2021-12-10 15:38:14 -0500

    Checking Sulfuras, Hand of Ragnaros in the inventory - test fail

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -29,5 +29,12 @@ describe("Gilded Rose,", function() {
         }
       }
     });
+    it("Sulfuras, Hand of Ragnaros", function() {
+      for(let i=0;i<gilded_rose.items.length;i++) {
+        if(gilded_rose.items[i].name == "Sulfuras, Hand of Ragnaros") {
+          assert(gilded_rose.items[i].name != "Sulfuras, Hand of Ragnaros");
+        }
+      }
+    });
   });
 });
