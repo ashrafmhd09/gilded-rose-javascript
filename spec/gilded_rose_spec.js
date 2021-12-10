17e6d9697ad6d7c08f7607bb9d5e479a836e068b
Ashraf <ashraf@Ashrafs-Air.home>
2021-12-10 17:23:46 -0500

    Checking Sulfuras, Hand of Ragnaros change at all - test fail

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -170,5 +170,15 @@ describe("Gilded Rose,", function() {
       }
     });
   });
+  describe("Sulfuras is legendary item,", function() {
+    it("does not change at all", function() {
+      for(let i=0;i<gilded_rose.items.length;i++) {
+        if (gilded_rose.items[i].name == "Sulfuras, Hand of Ragnaros") {
+          assert(gilded_rose.items[i].sell_in > 0);
+          assert(gilded_rose.items[i].quality > 80);
+        }
+      }
+    })
+  });
 
 });
